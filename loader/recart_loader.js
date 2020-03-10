0b2368d91c594516e4ff671351e182c311e1fdac
gabor-recart <gabor.marosi@recart.com>
2020-03-10 10:16:50 +0100

    fixing loading scripts

--- loader/recart_loader.js
+++ loader/recart_loader.js
@@ -1,23 +1,32 @@
-
-
-const isTrackingNgrok = new RegExp("_trackingNgrok=true").test(document.location.search)
-const isMessengerWidgetNgrok = new RegExp("_messengerNgrok=true").test(document.location.search)
+const isTrackingNgrok = new RegExp('_trackingNgrok=true').test(document.location.search)
+const isMessengerWidgetNgrok = new RegExp('_messengerNgrok=true').test(document.location.search)
 
 console.log({isTrackingNgrok, isMessengerWidgetNgrok})
 
+function loadScript (src) {
+  const script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.src = src
+  document.head.appendChild(script)
+}
+
 if (!isTrackingNgrok) {
   console.log('loading tracking from production')
-  (function() { const script = document.createElement('script'); script.type = 'text/javascript'; script.src = 'https://cdn.ghostmonitor.com/loader.js?domain=gabor-test-prod.myshopify.com&account=5e660388497b74af2cd8ac15&hasGMInitParams=1&rand=' + Math.random(); document.head.appendChild(script); })()
+  const src = 'https://cdn.ghostmonitor.com/loader.js?domain=gabor-test-prod.myshopify.com&account=5e660388497b74af2cd8ac15&hasGMInitParams=1&rand=' + Math.random()
+  loadScript(src)
 } else {
   console.log('loading tracking from ngrok')
-  (function() { const script = document.createElement('script'); script.type = 'text/javascript'; script.src = 'https://gm-tracking-js-gabor.ngrok.io/loader.js?domain=gabor-test-prod.myshopify.com&account=5e660388497b74af2cd8ac15&hasGMInitParams=1&rand=' + Math.random(); document.head.appendChild(script); })()
+  const src = 'https://gm-tracking-js-gabor.ngrok.io/loader.js?domain=gabor-test-prod.myshopify.com&account=5e660388497b74af2cd8ac15&hasGMInitParams=1&rand=' + Math.random()
+  loadScript(src)
 }
 
 if (!isMessengerWidgetNgrok) {
   console.log('loading messenger from production')
-  (function() { const script = document.createElement('script'); script.type = 'text/javascript'; script.src = 'https://cdn.ghostmonitor.com/messenger/loader.js?rand=' + Math.random(); document.head.appendChild(script); })()
+  const src = 'https://cdn.ghostmonitor.com/messenger/loader.js?rand=' + Math.random()
+  loadScript(src)
 } else {
   console.log('loading messenger from ngrok')
-  (function() { const script = document.createElement('script'); script.type = 'text/javascript'; script.src = 'https://gm-widget-js-gabor.ngrok.io/messenger/loader.js?rand=' + Math.random(); document.head.appendChild(script); })()
+  const src = 'https://gm-widget-js-gabor.ngrok.io/messenger/loader.js?rand=' + Math.random()
+  loadScript(src)
 }
 
