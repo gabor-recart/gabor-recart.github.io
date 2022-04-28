6aceea1ba1ecc2bf76f17b772223309c3b69cb92
gabormarosi <gabor.marosi@recart.com>
2022-04-28 17:40:55 +0200

    build 04.28 17.39

---
+++ build/attentive.js
@@ -0,0 +1,38 @@
+/*! RECART 89f1010 2022-04-28T15:27:01.842Z */
+"use strict";
+(self["webpackChunkgm_tracking_js"] = self["webpackChunkgm_tracking_js"] || []).push([[0],{
+
+/***/ 490:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ init)
+/* harmony export */ });
+/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(270);
+
+function init(_ref) {
+  let {
+    window,
+    pixel
+  } = _ref;
+  const attentiveId = (0,_utils__WEBPACK_IMPORTED_MODULE_0__.getCookie)('__attentive_id');
+  let shouldSend = !!attentiveId;
+
+  if (shouldSend && window.sessionStorage) {
+    shouldSend = window.sessionStorage.getItem('recart__attentive_id') !== attentiveId;
+  }
+
+  if (!shouldSend) return;
+  pixel('attentive_id', {
+    attentiveId: attentiveId
+  });
+
+  if (window.sessionStorage) {
+    window.sessionStorage.setItem('recart__attentive_id', attentiveId);
+  }
+}
+
+/***/ })
+
+}]);
---
+++ build/loader.js
@@ -0,0 +1,32 @@
+;(function (document) {
+  const id = 'recart-tracking-js'
+  if (document.getElementById(id)) {
+    return
+  }
+
+  const container = document.createElement('div')
+  container.attachShadow({ mode: 'open' })
+  document.body.appendChild(container)
+  container.id = 'recart-root'
+
+  // Disable fastclick to avoid event collision
+  // https://github.com/ftlabs/fastclick/blob/f77afc87aa43fb7a89e1ed8ba20c8d1695bd95b6/README.md#ignore-certain-elements-with-needsclick
+  container.classList.add('needsclick')
+  container.style.display = 'block'
+
+  const link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.href = 'https://gm-tracking-js.ngrok.io/main.css'
+  document.querySelector('#recart-root').shadowRoot.appendChild(link)
+
+  const font = document.createElement('link')
+  font.rel = 'stylesheet'
+  font.href = 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
+  document.head.appendChild(font)
+
+  const script = document.createElement('script')
+  script.id = id
+  script.src = 'https://gm-tracking-js.ngrok.io/main.js'
+  script.async = true
+  document.body.appendChild(script)
+})(document)
